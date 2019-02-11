9aa20ade330522cb14dfc36de84ce3042f58bd65
tapleyn <ntapley@cartrawler.com>
2019-02-11 20:05:53 +0000

    changes for car detector

--- app/server.py
+++ app/server.py
@@ -8,10 +8,11 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1ZwlUK7YjTVF1g9wNLPhwE3jZJfte9BR'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['countach','f40']
 path = Path(__file__).parent
 
 app = Starlette()
