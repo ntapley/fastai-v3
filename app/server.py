ad3378971685495a7c5289873cc5159510d470de
tapleyn <ntapley@cartrawler.com>
2019-02-11 20:35:13 +0000

    quoted link

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 #export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = https://www.dropbox.com/s/r3x4dntkdsnf4vs/export.pkl?dl=1
+export_file_url = 'https://www.dropbox.com/s/r3x4dntkdsnf4vs/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['countach','f40'] #['teddys','grizzly','black']
